d2b07e682f879ade410de8c39fe814bb0b77aaac
Xennia Contreras <Xenbydesign@gmail.com>
2024-03-15 12:32:30 -0700

    done

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,16 @@
 # your code goes here!
+
+
+class Anagram:
+
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_list):
+        anagrams = [
+            w
+            for w in word_list
+            if sorted(w.lower()) == sorted(self.word.lower())
+            and w.lower() != self.word.lower()
+        ]
+        return anagrams
